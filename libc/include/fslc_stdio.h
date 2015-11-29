11d41770b99fe2d3f24c3f1f634ffb56010a3c63
Jurģis Brigmanis <velko.x@gmail.com>
2015-11-29 23:37:05 +0200

    Stub implementation of *printf() family

--- libc/include/fslc_stdio.h
+++ libc/include/fslc_stdio.h
@@ -1,6 +1,8 @@
 #ifndef FSLC_STDIO_H
 #define FSLC_STDIO_H
 
+#include <stdarg.h>
+
 #ifndef ALT_FSLC_NAMES
 
 #define FSLC_FILE       FILE
@@ -9,6 +11,11 @@
 #define fslc_fputc      fputc
 #define fslc_fputs      fputs
 #define fslc_puts       puts
+
+#define fslc_vfprintf   vfprintf
+#define fslc_vprintf    vprintf
+#define fslc_fprintf    fprintf
+#define fslc_printf     printf
 
 #endif /* ALT_FSLC_NAMES */
 
@@ -32,6 +39,11 @@ extern "C" {
     int fslc_fputc(int c, FSLC_FILE *stream);
     int fslc_fputs(const char *str, FSLC_FILE *stream);
     int fslc_puts(const char *str);
+
+    int fslc_vfprintf(FSLC_FILE *stream, const char *format, va_list arg);
+    int fslc_vprintf(const char *format, va_list arg);
+    int fslc_fprintf(FSLC_FILE *stream, const char *format, ...);
+    int fslc_printf(const char *format, ...);
 
 #ifdef __cplusplus
 } /* extern "C" */
---
+++ libc/src/fslc_vfprintf.c
@@ -0,0 +1,6 @@
+#include "fslc_stdio.h"
+
+int fslc_vfprintf(FSLC_FILE *stream, const char *format, va_list arg)
+{
+    return -1;
+}
