783583b5dfd18ced57d415eb15c64e003b208094
Jurģis Brigmanis <velko.x@gmail.com>
2015-12-06 14:52:35 +0200

    Special case for printing 64-bit ints

--- libc/src/fslc_vfprintf.c
+++ libc/src/fslc_vfprintf.c
@@ -1,8 +1,47 @@
 #include "fslc_stdio.h"
 
 int _fslc_fputs_impl(const char *str, FSLC_FILE *stream);
-int _fslc_put_sint(signed int v, FSLC_FILE *stream);
-int _fslc_put_uint(unsigned int v, FSLC_FILE *stream);
+int _fslc_put_sint_l(signed long v, FSLC_FILE *stream);
+int _fslc_put_uint_l(unsigned long v, FSLC_FILE *stream);
+
+/* There are some considerations when formatting Integers for output:
+ *     - on 32-bit systems integers will mostly be 32 bits long, except when one REALLY
+ *       wants to print long long. It should be more optimal to use 32-bit calculations
+ *       when possible and invoke 64-bit only when needed. To support that we provide 2
+ *       separate implementations.
+ *     - on 64-bit systems may be 32 or 64 bits, but there is no real benefit from
+ *       using shorter bit size. Hence common implementation.
+ *     - this library does not aim to support systems with shorter integer sizes (like 16-bit)
+ *       but it it did, it would probably make sense to provide optimized ports for these cases
+ *       as well.
+ * 
+ * Buffer size calculations:
+ *   Max UInt32 fits in 10 bytes (+1 for zero terminator) decimal
+ *   2**32 - 1 == 4294967295z
+ *                FFFFFFFFz
+ *                01234567890
+ * 
+ *   Max UInt64 fits in 10 bytes (and +1 for \0)
+ *   2**64 -1  == 18446744073709551615z
+ *                FFFFFFFFFFFFFFFFz
+ *                012345678901234567890
+ */
+
+#define BUFSIZE_LONG_LONG_DECIMAL   20
+#if __SIZEOF_LONG__ == 4
+
+#define BUFSIZE_LONG_DECIMAL    10
+int _fslc_put_sint_ll(signed long long v, FSLC_FILE *stream);
+int _fslc_put_uint_ll(unsigned long long v, FSLC_FILE *stream);
+
+#elif __SIZEOF_LONG__ == 8
+
+#define BUFSIZE_LONG_DECIMAL    BUFSIZE_LONG_LONG_DECIMAL
+#define _fslc_put_sint_ll   _fslc_put_sint_l
+#define _fslc_put_uint_ll   _fslc_put_uint_l
+
+#endif
+
 
 static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, va_list arg)
 {
@@ -37,13 +76,13 @@ static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, va_list ar
                 
                 case 'i':
                 case 'd':
-                    pr = _fslc_put_sint(va_arg(arg, signed int), stream);
+                    pr = _fslc_put_sint_l(va_arg(arg, signed int), stream);
                     if (pr < 0) return pr;
                     res += pr;
                     break;
                 
                 case 'u':
-                    pr = _fslc_put_uint(va_arg(arg, unsigned int), stream);
+                    pr = _fslc_put_uint_l(va_arg(arg, unsigned int), stream);
                     if (pr < 0) return pr;
                     res += pr;
                     break;            }
@@ -68,32 +107,28 @@ int fslc_vfprintf(FSLC_FILE *stream, const char *format, va_list arg)
     return res;
 }
 
-int _fslc_put_sint(signed int v, FSLC_FILE *stream)
+int _fslc_put_sint_l(signed long v, FSLC_FILE *stream)
 {
     if (v < 0)
     {
         int pr = stream->putc('-', stream);
         if (pr < 0) return pr;
         
-        int vr = _fslc_put_uint(-v, stream);
-        if (vr < 0) return vr;
+        pr = _fslc_put_uint_l(-v, stream);
+        if (pr < 0) return pr;
         
-        return vr+1;
+        return pr+1;
     } else
-        return _fslc_put_uint(v, stream);
+        return _fslc_put_uint_l(v, stream);
 }
 
-int _fslc_put_uint(unsigned int v, FSLC_FILE *stream)
+int _fslc_put_uint_l(unsigned long v, FSLC_FILE *stream)
 {
-    /* Max UInt32 fits in 10 bytes (+1 for zero terminator)
-     * 2**32 == 4294967296z
-     *          01234567890
-     */
-    char dbuff[11], *p;
+    char dbuff[BUFSIZE_LONG_DECIMAL+1], *p;
 
-    dbuff[10] = 0;
+    dbuff[BUFSIZE_LONG_DECIMAL] = 0;
 
-    for (p = dbuff + 10; v ; v /= 10)
+    for (p = dbuff + BUFSIZE_LONG_DECIMAL; v ; v /= 10)
     {
         *(--p) = (v % 10) + '0';
     }
@@ -102,3 +137,38 @@ int _fslc_put_uint(unsigned int v, FSLC_FILE *stream)
 
     return _fslc_fputs_impl(p, stream);
 }
+
+#if __SIZEOF_LONG__ == 4
+
+int _fslc_put_sint_ll(signed long long v, FSLC_FILE *stream)
+{
+    if (v < 0)
+    {
+        int pr = stream->putc('-', stream);
+        if (pr < 0) return pr;
+        
+        pr = _fslc_put_uint_l(-v, stream);
+        if (pr < 0) return pr;
+        
+        return pr+1;
+    } else
+        return _fslc_put_uint_ll(v, stream);
+}
+
+int _fslc_put_uint_ll(unsigned long long v, FSLC_FILE *stream)
+{
+    char dbuff[BUFSIZE_LONG_LONG_DECIMAL+1], *p;
+
+    dbuff[BUFSIZE_LONG_LONG_DECIMAL] = 0;
+
+    for (p = dbuff + BUFSIZE_LONG_LONG_DECIMAL; v ; v /= 10)
+    {
+        *(--p) = (v % 10) + '0';
+    }
+
+    if (*p == 0) *(--p) = '0';
+
+    return _fslc_fputs_impl(p, stream);
+}
+
+#endif
