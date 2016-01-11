5f7ff9abd90f67bba086945849780079e2211f72
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-11 17:46:55 +0200

    Wrapping va_list in struct va_list_w - to be able to pass it around freely

--- libc/src/fslc_vfprintf.c
+++ libc/src/fslc_vfprintf.c
@@ -1,14 +1,40 @@
 #include "fslc_stdio.h"
 
+/* There'a a funny behaviour of va_list and va_arg() macros:
+ *    - on x86_64 compiler generates a branch of some sort for each va_arg() macro. Normally it is not a problem,
+ *      however it kinda screws up branch coverage analysis. Therefore I decided to move va_arg() accesses into
+ *      dedicated functions, minimizing the issue to one untaken branch per access function.
+ *    - passing va_list by value works fine on x86_64, but makes some tests to fail on i386. It seems that modifications
+ *      done by va_arg() is not passed back to caller. Subsequent calls to va_arg() or access functions returns wrong
+ *      values on that platform.
+ *    - OK, then. Let's pass pointer to va_list instead. Not so fast. Works fine on i386, but fails to compile on
+ *      x86_64. Fine! Wrapping va_list into a dedicated struct and passing around pointer to it.
+ *
+ *    Why such a fuss about a few untaken branches in tests? Well, just because! Also, if I ever decide to refactor
+ *    _fslc_vfprintf_impl() - the case statement is getting a bit long and messy, it should make things easier if
+ *    argument list can be passed around freely (as opposed to using it from single function).
+ */
+
+struct va_list_w
+{
+    va_list arg;
+};
+
 /* Prototype for printf's internal implementation */
-static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, va_list arg);
+static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, struct va_list_w *args);
+
 
 /* Main exported function */
 int fslc_vfprintf(FSLC_FILE *stream, const char *format, va_list arg)
 {
     if (fslc_stdout->pre_output) fslc_stdout->pre_output(fslc_stdout);
 
-    int res = _fslc_vfprintf_impl(stream, format, arg);
+    struct va_list_w argw;
+    va_copy(argw.arg, arg); // can not assign directly, should make a copy
+
+    int res = _fslc_vfprintf_impl(stream, format, &argw);
+
+    va_end(argw.arg); // and free it after use
 
     if (fslc_stdout->post_output) fslc_stdout->post_output(fslc_stdout);
 
@@ -24,11 +50,11 @@ static int _fslc_put_sint_l(signed long v, FSLC_FILE *stream);
 static int _fslc_put_uint_l(unsigned long v, FSLC_FILE *stream);
 static int _fslc_put_hex_l(unsigned long v, FSLC_FILE *stream, char alpha);
 
-static int _get_sint_arg(va_list arg);
-static unsigned _get_uint_arg(va_list arg);
-static long long _get_slonglong_arg(va_list arg);
-static unsigned long long _get_ulonglong_arg(va_list arg);
-static void *_get_ptr_arg(va_list arg);
+static int _get_sint_arg(struct va_list_w *arg);
+static unsigned _get_uint_arg(struct va_list_w *arg);
+static long long _get_slonglong_arg(struct va_list_w *arg);
+static unsigned long long _get_ulonglong_arg(struct va_list_w *arg);
+static void *_get_ptr_arg(struct va_list_w *arg);
 
 /* There are some considerations when formatting Integers for output:
  *     - on 32-bit systems integers will mostly be 32 bits long, except when one REALLY
@@ -81,7 +107,7 @@ static int _fslc_put_hex_ll(unsigned long long v, FSLC_FILE *stream, char alpha)
 #define FLAG_LONG       1
 #define FLAG_VERYLONG   3
 
-static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, va_list arg)
+static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, struct va_list_w *arg)
 {
     int res = 0;
     int pr;
@@ -195,29 +221,29 @@ static int _fslc_vfprintf_impl(FSLC_FILE *stream, const char *format, va_list ar
     return res;
 }
 
-static int _get_sint_arg(va_list arg)
+static int _get_sint_arg(struct va_list_w *arg)
 {
-    return va_arg(arg, signed int);
+    return va_arg(arg->arg, signed int);
 }
 
-static unsigned _get_uint_arg(va_list arg)
+static unsigned _get_uint_arg(struct va_list_w *arg)
 {
-    return va_arg(arg, unsigned int);
+    return va_arg(arg->arg, unsigned int);
 }
 
-static long long _get_slonglong_arg(va_list arg)
+static long long _get_slonglong_arg(struct va_list_w *arg)
 {
-    return va_arg(arg, signed long long);
+    return va_arg(arg->arg, signed long long);
 }
 
-static unsigned long long _get_ulonglong_arg(va_list arg)
+static unsigned long long _get_ulonglong_arg(struct va_list_w *arg)
 {
-    return va_arg(arg, unsigned long long);
+    return va_arg(arg->arg, unsigned long long);
 }
 
-static void *_get_ptr_arg(va_list arg)
+static void *_get_ptr_arg(struct va_list_w *arg)
 {
-    return va_arg(arg, void *);
+    return va_arg(arg->arg, void *);
 }
 
 static int _fslc_put_sint_l(signed long v, FSLC_FILE *stream)
