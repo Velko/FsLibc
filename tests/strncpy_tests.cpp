0e831dbf294b63e80b7ab46f33eb7075015331de
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-23 18:43:00 +0200

    Tests for strncpy()

---
+++ tests/strncpy_tests.cpp
@@ -0,0 +1,27 @@
+#include <UnitTest++.h>
+
+#include "strmod_fixture.h"
+#include <string.h>
+
+SUITE(StrNCpy)
+{
+    TEST_FIXTURE(StrModFixture, Copy0TerminateTest)
+    {
+        char *cc = fslc_strncpy(testString,"Hello, World!", 30);
+        strncpy(expectedString,"Hello, World!", 30);
+
+        CHECK_EQUAL((uintptr_t)testString, (uintptr_t)cc);
+
+        CHECK_ARRAY_EQUAL(expectedString, testString, MAXLEN);
+    }
+
+    TEST_FIXTURE(StrModFixture, CopyNTerminateTest)
+    {
+        char *cc = fslc_strncpy(testString,"Hello, World!", 5);
+        strncpy(expectedString,"Hello, World!", 5);
+
+        CHECK_EQUAL((uintptr_t)testString, (uintptr_t)cc);
+
+        CHECK_ARRAY_EQUAL(expectedString, testString, MAXLEN);
+    }
+}
