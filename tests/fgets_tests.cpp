2a6fa5006b9be494f2de1c3c16d37628cd427be2
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-23 11:54:10 +0200

    Additional fgets() tests CRLF handling and reading second line

--- tests/fgets_tests.cpp
+++ tests/fgets_tests.cpp
@@ -18,7 +18,7 @@ SUITE(FGets)
         CHECK_EQUAL(5, strlen(str));
         CHECK_EQUAL("Hello", std::string(str));
     }
-    
+
     TEST_FIXTURE(StdIOFixture, FGetsNewLineTest)
     {
         istring.str("Hello\nWorld");
@@ -45,5 +45,50 @@ SUITE(FGets)
         CHECK_EQUAL(0, str[5]);
         CHECK_EQUAL(5, strlen(str));
         CHECK_EQUAL("Hello", std::string(str));
+    }
+
+    TEST_FIXTURE(StdIOFixture, FGetsCrLfLineTest)
+    {
+        istring.str("Hello\r\nWorld");
+
+        char text_read[20];
+
+        char *str = fslc_fgets(text_read, 20, &stream);
+
+        CHECK_EQUAL(text_read, str);
+        CHECK_EQUAL(0, str[7]);
+        CHECK_EQUAL(7, strlen(str));
+        CHECK_EQUAL("Hello\r\n", std::string(str));
+    }
+
+    TEST_FIXTURE(StdIOFixture, FGetsAfterNewLineTest)
+    {
+        istring.str("Hello\nWorld");
+
+        char text_read[20];
+
+        fslc_fgets(text_read, 20, &stream);
+        char *str = fslc_fgets(text_read, 20, &stream);
+
+
+        CHECK_EQUAL(text_read, str);
+        CHECK_EQUAL(0, str[5]);
+        CHECK_EQUAL(5, strlen(str));
+        CHECK_EQUAL("World", std::string(str));
+    }
+
+    TEST_FIXTURE(StdIOFixture, FGetsAfterCrLfLineTest)
+    {
+        istring.str("Hello\r\nWorld");
+
+        char text_read[20];
+
+        fslc_fgets(text_read, 20, &stream);
+        char *str = fslc_fgets(text_read, 20, &stream);
+
+        CHECK_EQUAL(text_read, str);
+        CHECK_EQUAL(0, str[5]);
+        CHECK_EQUAL(5, strlen(str));
+        CHECK_EQUAL("World", std::string(str));
     }
 }
