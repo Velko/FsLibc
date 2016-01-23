c6dac6cd4a03e47408a9e2b70faf27cae28661dd
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-23 15:51:26 +0200

    strcmp() tests

---
+++ tests/misc_utils.h
@@ -0,0 +1,6 @@
+#ifndef MISC_UTILS
+#define MISC_UTILS
+
+#define SIGNOF(X)   ((X) > 0 ? 1 : ((X) < 0 ? -1 : 0))
+
+#endif
---
+++ tests/strcmp_tests.cpp
@@ -0,0 +1,64 @@
+#include <UnitTest++.h>
+#define ALT_FSLC_NAMES /* use alternative FSLC names - do not clash with Host Libc */
+#include "fslc_string.h"
+#include <string.h>
+
+#include "misc_utils.h"
+
+SUITE(StrCmp)
+{
+    TEST(EqualTest)
+    {
+        const char *str1 = "Test string";
+        const char *str2 = "Test string";
+
+        int r = fslc_strcmp(str1, str2);
+        int e = strcmp(str1, str2);
+
+        CHECK_EQUAL(e, r);
+    }
+
+    TEST(LargerTest)
+    {
+        const char *str1 = "Test string 1";
+        const char *str2 = "Test string 2";
+
+        int r = fslc_strcmp(str1, str2);
+        int e = strcmp(str1, str2);
+
+        CHECK_EQUAL(SIGNOF(e) , SIGNOF(r));
+    }
+
+    TEST(SmallerTest)
+    {
+        const char *str1 = "Test string 1";
+        const char *str2 = "Test string 0";
+
+        int r = fslc_strcmp(str1, str2);
+        int e = strcmp(str1, str2);
+
+        CHECK_EQUAL(SIGNOF(e) , SIGNOF(r));
+    }
+
+    TEST(LongerTest)
+    {
+        const char *str1 = "Test string 1";
+        const char *str2 = "Test string";
+
+        int r = fslc_strcmp(str1, str2);
+        int e = strcmp(str1, str2);
+
+        CHECK_EQUAL(SIGNOF(e) , SIGNOF(r));
+    }
+
+    TEST(ShorterTest)
+    {
+        const char *str1 = "Test string";
+        const char *str2 = "Test string 2";
+
+        int r = fslc_strcmp(str1, str2);
+        int e = strcmp(str1, str2);
+
+        CHECK_EQUAL(SIGNOF(e) , SIGNOF(r));
+    }
+}
