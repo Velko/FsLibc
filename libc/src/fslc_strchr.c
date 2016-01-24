c0359e0cc4501d07e93022c205ba6d57891a941a
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-24 17:51:08 +0200

    Standalone implementation of strchr()

--- libc/src/fslc_strchr.c
+++ libc/src/fslc_strchr.c
@@ -1,7 +1,10 @@
 #include "fslc_string.h"
-#include <string.h>
 
 char *fslc_strchr(const char *str, int c)
 {
-    return strchr(str, c);
+    for (;*str; ++str)
+        if (*str == c)
+            return (char *)str;
+
+    return NULL;
 }
