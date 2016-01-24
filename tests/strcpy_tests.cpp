eaa8714adda8e630c14777795526f42d502b5253
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-24 16:41:46 +0200

    Additional test for strcpy_e() - check return value

--- tests/strcpy_tests.cpp
+++ tests/strcpy_tests.cpp
@@ -14,4 +14,14 @@ SUITE(StrCpy)
 
         CHECK_ARRAY_EQUAL(expectedString, testString, MAXLEN);
     }
+
+    TEST_FIXTURE(StrModFixture, StrCpyEResultTest)
+    {
+        char *cc = fslc_strcpy_e(testString,"Hello, World!");
+        strcpy(expectedString,"Hello, World!");
+
+        CHECK_EQUAL((uintptr_t)testString+strlen(expectedString), (uintptr_t)cc);
+
+        CHECK_ARRAY_EQUAL(expectedString, testString, MAXLEN);
+    }
 }
