eff6426e6c1794809ecef2e5f868dc0e3d104140
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-24 19:41:07 +0200

    Standalone implementation of strstr() and additional tests to cover all cases

--- tests/strstr_tests.cpp
+++ tests/strstr_tests.cpp
@@ -20,8 +20,38 @@ SUITE(StrStr)
         const char *txt = "Test string";
 
         const char *r = fslc_strstr(txt, "m");
-        const char *e = strstr(txt, "mn");
+        const char *e = strstr(txt, "m");
 
         CHECK_EQUAL((uintptr_t)e, (uintptr_t)r); // compare pointer values
+    }
+
+    TEST(FindSecond)
+    {
+        const char *txt = "Test text";
+
+        const char *r = fslc_strstr(txt, "te");
+        const char *e = strstr(txt, "te");
+
+        CHECK_EQUAL((uintptr_t)e, (uintptr_t)r);
+    }
+
+    TEST(FindSecondLongerTest)
+    {
+        const char *txt = "Test string";
+
+        const char *r = fslc_strstr(txt, "str");
+        const char *e = strstr(txt, "str");
+
+        CHECK_EQUAL((uintptr_t)e, (uintptr_t)r);
+    }
+
+    TEST(NotFoundAfter3)
+    {
+        const char *txt = "Test text";
+
+        const char *r = fslc_strstr(txt, "tu");
+        const char *e = strstr(txt, "tu");
+
+        CHECK_EQUAL((uintptr_t)e, (uintptr_t)r);
     }
 }
