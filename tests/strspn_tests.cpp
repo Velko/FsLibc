35439124f2a026bc990622a37429c2194fd2230e
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-24 21:46:25 +0200

    Unit tests for strspn()

---
+++ tests/strspn_tests.cpp
@@ -0,0 +1,37 @@
+#include <UnitTest++.h>
+#define ALT_FSLC_NAMES /* use alternative FSLC names - do not clash with Host Libc */
+#include "fslc_string.h"
+#include <string.h>
+
+SUITE(StrSpn)
+{
+    TEST(BeginningTest)
+    {
+        const char *txt = "*^*#^*The text continues";
+
+        size_t r = fslc_strspn(txt, "#*^");
+        size_t e = strspn(txt, "#*^");
+
+        CHECK_EQUAL(e, r);
+    }
+
+    TEST(ZeroResTest)
+    {
+        const char *txt = "This is *^*#^*";
+
+        size_t r = fslc_strspn(txt, "#*^");
+        size_t e = strspn(txt, "#*^");
+
+        CHECK_EQUAL(e, r);
+    }
+
+    TEST(AllTest)
+    {
+        const char *txt = "*^*#^*^^**###";
+
+        size_t r = fslc_strspn(txt, "#*^");
+        size_t e = strspn(txt, "#*^");
+
+        CHECK_EQUAL(e, r);
+    }
+}
