46a6aac073a40c3afe39e60a90ade644ced109f5
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-25 18:27:32 +0200

    Split up strncpy - one function per file

--- libc/src/fslc_strncpy.c
+++ libc/src/fslc_strncpy.c
@@ -1,24 +1,9 @@
 #include "fslc_string.h"
 
-static char *_fslc_strncpy_impl(char *dest, const char *src, size_t len);
+char *_fslc_strncpy_impl(char *dest, const char *src, size_t len);
 
 char *fslc_strncpy(char *dest, const char *src, size_t len)
 {
     _fslc_strncpy_impl(dest, src, len);
-    return dest;
-}
-
-char *fslc_strncpy_e(char *dest, const char *src, size_t len)
-{
-    char *r = _fslc_strncpy_impl(dest, src, len);
-    return *r ? r + 1: r;
-}
-
-static char *_fslc_strncpy_impl(char *dest, const char *src, size_t len)
-{
-    for(; *src && len > 1; ++src, ++dest, --len)
-        *dest = *src;
-
-    *dest = *src;
     return dest;
 }
