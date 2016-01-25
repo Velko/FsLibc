e4a24e2d5a55d3b10850d9f63816fef70a65b11d
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-25 18:18:36 +0200

    Split up memset - one function per file

--- libc/src/fslc_memset.c
+++ libc/src/fslc_memset.c
@@ -1,6 +1,5 @@
 #include "fslc_string.h"
 #include "fslc_stringx.h"
-#include <stdint.h>
 
 void *fslc_memset(void *ptr, int value, size_t num)
 {
@@ -20,33 +19,4 @@ void *fslc_memset(void *ptr, int value, size_t num)
     }
     else
         return fslc_memset_l(ptr, value, num);
-}
-
-void *fslc_memset_l(void *ptr, unsigned long value, size_t num)
-{
-    unsigned char *b_src = (unsigned char *)&value;
-    unsigned char *b_dst = (unsigned char *)ptr;
-
-    /* Byte by byte - to aligned word address */
-    b_src += ((uintptr_t)b_dst & (sizeof(unsigned long)-1));
-    for (;((uintptr_t)b_dst & (sizeof(unsigned long)-1)) && num > 0; b_dst++,b_src++,num--)
-    {
-        *b_dst = *b_src;
-    }
-
-    /* All aligned */
-    unsigned long *w_dst = (unsigned long *)b_dst;
-    for (;num >= sizeof(unsigned long); w_dst++,num -= sizeof(unsigned long))
-    {
-        *w_dst= value;
-    }
-
-    /* Unaligned ending */
-    b_src = (unsigned char *)&value;
-    b_dst = (unsigned char *)w_dst;
-    for (; num > 0; b_dst++,b_src++,num--)
-    {
-        *b_dst = *b_src;
-    }
-    return ptr;
 }
