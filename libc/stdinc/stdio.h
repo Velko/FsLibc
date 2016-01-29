c2f71b9fc8750857c229ebca1f954257eb7e8009
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-29 20:52:27 +0200

    Add standard include files

---
+++ libc/stdinc/stdio.h
@@ -0,0 +1,6 @@
+#ifndef _STD_STDIO_H
+#define _STD_STDIO_H
+
+#include "fslc/fslc_stdio.h"
+
+#endif /* _STD_STDIO_H */
---
+++ libc/stdinc/string.h
@@ -0,0 +1,6 @@
+#ifndef _STD_STRING_H
+#define _STD_STRING_H
+
+#include "fslc/fslc_string.h"
+
+#endif /* _STD_STRING_H */
