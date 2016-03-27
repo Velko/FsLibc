951cb4480c2a94ad80332e0a53c371a2eee0583f
Jurģis Brigmanis <velko.x@gmail.com>
2016-03-27 19:59:25 +0300

    Generate coverage report from CMake-built objects

--- libc/coverage.sh
+++ libc/coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-lcov -c --rc lcov_branch_coverage=1 -d Debug -o Debug/coverage.info
-genhtml Debug/coverage.info --branch-coverage -o cov_report
+lcov -c --rc lcov_branch_coverage=1 -d CMakeFiles/fslc.dir/src -o coverage.info
+genhtml coverage.info --branch-coverage -o cov_report
