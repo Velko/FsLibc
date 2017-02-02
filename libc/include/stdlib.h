12c86482c03b5c57a01b4fe3166a6580ee8c9a50
Jurģis Brigmanis <velko.x@gmail.com>
2017-02-02 20:33:28 +0200

    Add bsearch() stub

---
+++ libc/include/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef _STD_STDLIB_H
+#define _STD_STDLIB_H
+
+#include "fslc/fslc_stdlib.h"
+
+#endif /* _STD_STDLIB_H */
---
+++ libc/src/fslc_bsearch.c
@@ -0,0 +1,6 @@
+#include "fslc_stdlib.h"
+
+void *fslc_bsearch(const void *key, const void *base, size_t num, size_t size, int (*cmp_proc)(const void *, const void *))
+{
+    return NULL;
+}
